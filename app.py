aad4741c0d84126b1a20bfae030cf89f020decf4
Kurt Oyewole <kurtoye_@hotmail.com>
2024-12-06 17:15:21 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, jsonify
 from services.inquiries_service import inquiries_bp
 from services.requests_service import requests_bp
->>>>>>> d1e1399fa0a83d501a93bdceb430038885f91d51
+
+import firebase_admin
+from firebase_admin import credentials, auth
+
+# Initialse Firebase Authentication
+cred = credentials.Certificate ('graceful-cider-438814-e0-firebase-adminsdk-8958u-a2289d9f51')
+firebase_admin.initialize_app(cred)
 
 # Initialize Flask app
 app = Flask(__name__)
